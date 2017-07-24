703295fd55bcfb7699070d3ca467f1593dc1501d
Amit Deshmukh <addeshmu@usc.edu>
2017-07-23 21:59:26 -0700

    Adding circle config

---
+++ one.go
@@ -0,0 +1,11 @@
+package one
+
+func one(b int, a int) bool {
+	if b != 2 {
+		if b+a > 10 {
+			return true
+		}
+		return false
+	}
+	return true
+}
---
+++ one_test.go
@@ -0,0 +1,25 @@
+package one
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//TestOneFalse is test for one.go
+func TestOneFalse(t *testing.T) {
+	if one(3, 4) != false {
+		t.Error("Wrong bool value is coming from one.go")
+	}
+}
+
+//TestOneTrue will check true being returned
+func TestOneTrue(t *testing.T) {
+	if one(23, 4) != true {
+		t.Error("Wrong bool value is coming from one.go")
+	}
+}
+
+func TestOneb2(t *testing.T) {
+	assert.Equal(t, one(2, 2), true, "this should be true")
+}
